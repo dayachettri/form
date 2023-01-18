92827d6b81a965c368838cd0d4f3ccdbe978eb99
Daya Chettri <Dayasagar073@gmail.com>
2023-01-18 15:00:50 +0530

    Manage state of the form inputs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [values, setValues] = useState({
+    fname: '',
+    lname: '',
+    email: '',
+    password: '',
+  });
+  console.log(values);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    const updatedValues = {
+      ...values,
+      [name]: value,
+    };
+
+    setValues(updatedValues);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setValues({
+      fname: '',
+      lname: '',
+      email: '',
+      password: '',
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section id="container">
+      <div className="right">
+        <form onSubmit={handleSubmit}>
+          <p className="trial-offer">
+            <strong> Sign Up</strong> right now
+          </p>
+          <div className="input-wrapper">
+            <input
+              value={values.fname}
+              onChange={handleChange}
+              type="text"
+              name="fname"
+              id="fname"
+              placeholder="First Name"
+              required
+            />
+            <input
+              value={values.lname}
+              onChange={handleChange}
+              type="text"
+              name="lname"
+              id="lname"
+              placeholder="Last Name"
+              required
+            />
+            <input
+              value={values.email}
+              onChange={handleChange}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email Address"
+              required
+            />
+            <input
+              value={values.password}
+              onChange={handleChange}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              required
+            />
+            <input type="submit" value="sign up" id="submit" />
+            <p>By clicking the button, you are agreeing to our</p>
+            <p>
+              <a href="https://www.google.com">Terms and conditions</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 }
 
